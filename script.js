7024b4bcb1558181faa2482d3e55a4eee8e7b17f
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-09-21 18:30:33 -0500

    JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const insert = document.getElementById('insert')
+
 window.addEventListener('keydown', (e) => {
-    console.log(e)
+    insert.innerHTML = `
+<div class="key">
+    ${e.key === ' ' ? 'Space': e.key}
+    <small>event.key</small>
+</div>
+<div class="key">
+    ${e.keyCode === ' ' ? 'Space': e.keyCode}  
+    <small>event.keyCode</small>
+</div>
+<div class="key">
+    ${e.code === ' ' ? 'Space': e.code}    
+    <small>event.code</small>
+</div>`
 })
